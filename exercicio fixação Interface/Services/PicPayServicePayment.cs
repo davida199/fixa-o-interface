bef85bcb600b080d30801bdf82774e2c5d976b8b
Davi Costa <davyda199@gmail.com>
2021-08-06 14:12:55 -0300

    implementando ContractService

--- exercicio fixação Interface/Services/PicPayServicePayment.cs
+++ exercicio fixação Interface/Services/PicPayServicePayment.cs
@@ -6,10 +6,14 @@ namespace exercicio_fixação_Interface.Services
 {
     class PicPayServicePayment : IServicePayment
     {
-        public double tax(int amount)
+        public double paymentFee(double amount)
         {
-            double acum = (amount * (1 / 100)) + (amount * (2 / 100));
-            return acum;
+            double acum = amount + (amount * (1 / 100));
+            return acum +=  (acum * (2 / 100));
+        }
+        public double interest(double amount, int months)
+        {
+            return paymentFee(amount) * months;
         }
     }
 }
